8e4ecf7c0c15abb0513efcc7676b34122e533f72
jguomoto <xuguo@google.com>
2020-02-10 22:45:15 -0800

    re-implement data helper

---
+++ data_helper.py
@@ -0,0 +1,78 @@
+import os
+import numpy as np
+import h5py
+from PIL import Image
+import random
+# import matplotlib.pyplot as plt
+
+# change this to your local path
+NYU_FILE_PATH = '/Users/jasonguo/Downloads/nyu_depth_v2_labeled.mat'
+MAX_DEPTH_METER = 6.0
+
+def convert_nyu(path):
+    if not os.path.isfile(path):
+        print('file not exist: ' + NYU_FILE_PATH)
+        return
+
+    print('Load dataset: %s' % (path))
+    h5file = h5py.File(path, 'r')
+
+    # # print all mat variabls:
+    # # (u'depths', <HDF5 dataset "depths": shape (1449, 640, 480), type "<f4">)
+    # # (u'images', <HDF5 dataset "images": shape (1449, 3, 640, 480), type "|u1">)
+    # for item in h5file.items():
+    #     print(str(item))
+
+    # image = h5file['images'][0]
+    # # transpose to w, h, channel layout
+    # image = np.transpose(image, (2, 1, 0))
+
+    # depth = h5file['depths'][0]
+    # # transpose to w, h layout
+    # depth = np.transpose(depth, (1, 0))
+    
+    # _, axarr = plt.subplots(2, sharex=True)
+    # axarr[0].imshow(image)
+    # axarr[1].imshow(depth)
+    # plt.show()
+
+    # training example contain a list of rgb depth pairs.
+    train_examples = []
+
+    if not os.path.isdir('data'):
+        os.mkdir('data')
+
+    for i in range(h5file['images'].shape[0]):
+        image = np.transpose(h5file['images'][i], (2, 1, 0))
+        depth = np.transpose(h5file['depths'][i], (1, 0))
+
+        # Do not use max depth to scale, instead use constant MAX_DEPTH_METER
+        # to make sure all depth image are distance is scaled proportionally.
+        depth = (depth / MAX_DEPTH_METER) * 255.0
+
+        image_name = os.path.join('data', '%05d_c.png' % (i))
+        depth_name = os.path.join('data', '%05d_d.png' % (i))
+        train_examples.append((image_name, depth_name))
+
+        # save to local png file.
+        image_im = Image.fromarray(np.uint8(image))
+        image_im.save(image_name)
+
+        depth_im = Image.fromarray(np.uint8(depth))
+        depth_im.save(depth_name)
+
+        print('saving file: %i' % i)
+        if i is 10:
+            break
+
+    # write train_examples to csv
+    with open('data/train.csv', 'w') as output:
+        for (image_name, depth_name) in train_examples:
+            output.write("%s,%s" % (image_name, depth_name))
+            output.write("\n")
+
+def main(argv=None):
+    convert_nyu(NYU_FILE_PATH)
+
+if __name__ == '__main__':
+    main()
