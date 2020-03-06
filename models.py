d26511e039a43db21e793f516fadfa915dd82f68
Meijiao Png <meijiao@gmail.com>
2020-03-05 19:25:11 -0800

    change models to functional instead of sequential

--- models.py
+++ models.py
@@ -44,12 +44,12 @@ def model2():
     return model
 
 def model2Functional():
-    first_layer=Input(shape=(IMAGE_WIDTH, IMAGE_HEIGHT, 3))
+    first_layer=Input(shape=(IMAGE_HEIGHT, IMAGE_WIDTH, 3))
     conv1 = Conv2D(96,(11,11),strides=(4,4),activation="relu",padding="same")(first_layer)
     batch1 = BatchNormalization()(conv1)
     pool1 = MaxPooling2D(pool_size=(2,2))(batch1)
     
-    conv2 = Conv2D(256, (5, 5), activation="relu", padding="same")(pool1))
+    conv2 = Conv2D(256, (5, 5), activation="relu", padding="same")(pool1)
     batch2 = BatchNormalization()(conv2)
     pool2 = MaxPooling2D(pool_size=(2,2))(batch2)
 
@@ -57,7 +57,7 @@ def model2Functional():
     batch3 = BatchNormalization()(conv3)
 
     conv4 = Conv2D(384, (3, 3), activation="relu", padding="same")(batch3)
-    batch4 = BatchNormalization()(batch4)
+    batch4 = BatchNormalization()(batch3)
 
     dense5 = Dense(256, activation="relu")(batch4)
     batch5 = BatchNormalization()(dense5)
@@ -71,11 +71,11 @@ def model2Functional():
     reshaped=Reshape((64,64,1))(flatten)
     upsampled=UpSampling2D(size=(2,2))(reshaped)
 
-    out1 = Conv2D(1, (55, 74), padding="valid")(upsampled)
+    out1 = Conv2D(1, (74, 55), padding="valid")(upsampled)
     out1 = BatchNormalization()(out1)
     model = Model(inputs=first_layer, outputs=out1)
     model.summary
-    return model
+    return model, first_layer, out1
 
 def model1():
     model = Sequential()
@@ -108,36 +108,25 @@ def model1():
     model.summary()
     return model
 
-def refinedNetworkModel(coarse_training_results):
-    layer1 = Input(shape=(IMAGE_WIDTH, IMAGE_HEIGHT, 3)) 
+def refinedNetworkModel():
+    coarse_model, layer1, coarse_training_results = model2Functional()
 
     conv1 = (Conv2D(filters = 63, kernel_size= (9, 9),
                     strides=(2, 2),
-                    padding="valid",
-                    input_shape=[IMAGE_WIDTH, IMAGE_HEIGHT, 3]))(layer1)
+                    padding="valid"))(layer1)
     batch1 = BatchNormalization()(conv1)
     pool1 = MaxPooling2D(pool_size=(2,2))(batch1)
 
-    #convert sequential model to functional so that we can concatenate
-    coarse_input = Input(shape=(IMAGE_WIDTH, IMAGE_HEIGHT, 3))
-    # verify how to convert input 
-    # https://stackoverflow.com/questions/50837715/conversion-of-a-sequential-model-to-a-functional-model-with-keras-2-2-0/50937113#50937113
-    coarse_layer = coarse_input
-    for layer in coarse_training_results.layers:
-        coarse_layer = layer(coarse_layer)
-     
-    functional_coarse_model = Model([coarse_layer, coarse_layer])
-
-    merged = concatenate([functional_coarse_model, pool1]) 
+    merged = concatenate([coarse_training_results, pool1]) 
     conv2 = Conv2D(filters = 64, kernel_size=(5, 5), padding="same")(merged)
     batch2 = BatchNormalization()(conv2)
 
     out = Conv2D(filters = 1, kernel_size=(5, 5), padding="same")(batch2)
     out = BatchNormalization()(out)
 
-    model = Model(inputs=[layer1, functional_coarse_model], outputs=out)
-    model.summary()
-    return model
+    refined_model = Model(inputs=layer1, outputs=out)
+    refined_model.summary()
+    return coarse_model, refined_model
 
 # def refinedNetworkModelSequential(coarse_training_results):
 #     first_layer = Sequential()
--- train.py
+++ train.py
@@ -22,9 +22,12 @@ TARGET_HEIGHT = 55
 TARGET_WIDTH = 74
 
 COARSE_CHECKPOINT_PATH = 'checkpoints/coarse/coarse_ckpt'
+COARSE_CHECKPOINT_DIR = os.path.dirname(COARSE_CHECKPOINT_PATH)
 REFINED_CHECKPOINT_PATH = 'checkpoints/refined/refined_ckpt'
+REFINED_CHECKPOINT_DIR = os.path.dirname(REFINED_CHECKPOINT_PATH)
+PREDICT_FILE_PATH = 'data/predict'
 
-is_refine_training = True
+RUN_REFINE = True
 
 class NyuDepthGenerator(keras.utils.Sequence) :
 
@@ -51,8 +54,8 @@ class NyuDepthGenerator(keras.utils.Sequence) :
             example = Image.open(pairs[0])
             label = Image.open(pairs[1])
 
-            example = example.resize((IMAGE_HEIGHT, IMAGE_WIDTH))
-            label = label.resize((TARGET_HEIGHT, TARGET_WIDTH))
+            example = example.resize((IMAGE_WIDTH, IMAGE_HEIGHT))
+            label = label.resize((TARGET_WIDTH, TARGET_HEIGHT))
 
             x_train.append(np.array(example))
             # flatten is needed because of the dense layer output is 1d
@@ -102,73 +105,138 @@ def msr_loss(y_true, y_pred):
     return loss
 
 
+# def main():
+#     print(tf.__version__)
+#
+#     # Pick a model (coarse or refined)
+#     if (is_refine_training):
+#         current_filepath = REFINED_CHECKPOINT_PATH,
+#         coarse_training_results = models.model2()
+#         model = models.refinedNetworkModel(coarse_training_results)
+#     else:
+#         current_filepath = COARSE_CHECKPOINT_PATH
+#         model = models.model2()
+#
+#     # Create a callback that saves the model's weights with every epoch
+#     cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=current_filepath, save_weights_only=True,verbose=1, save_freq='epoch')
+#
+#     latest_checkpoint = tf.train.latest_checkpoint(os.path.dirname(current_filepath))
+#
+#     # If training refined for the first time, no checkpoint will be found in refined dir.
+#     # Restore from coarse directory.
+#     if is_refined_training and not latest_checkpoint:
+#         latest_checkpoint = tf.train.latest_checkpoint(os.path.dirname(COARSE_CHECKPOINT_PATH))
+#         print("\nGetting coarse checkpoint for refined training")
+#         model.load_weights(latest_checkpoint)
+#     elif (latest_checkpoint):
+#         print("\nRestoring form checkpoint")
+#         model.load_weights(latest_checkpoint)
+#     else:
+#         print("\nTraining model from scratch")
+#
+#     nyu_data_generator = NyuDepthGenerator(batch_size=10)
+#
+#     # parallel_model = multi_gpu_model(model, gpus=2)
+#     # parallel_model.compile(optimizer=keras.optimizers.Adam(),  # Optimizer
+#     #           # Loss function to minimize
+#     #           loss=msr_loss,
+#     #           # List of metrics to monitor
+#     #           metrics=None)
+#
+#     model.compile(optimizer=keras.optimizers.Adam(),  # Optimizer
+#               # Loss function to minimize
+#               loss=depth_loss,
+#               # List of metrics to monitor
+#               metrics=None)
+#
+#     print("model metric names: " + str(model.metrics_names))
+#
+#     print('# Fit model on training data')
+#     # when using data generate, x contains both X and Y.
+#     # batch size is define in the generator thus passing None to batch_size
+#     # https://www.tensorflow.org/api_docs/python/tf/keras/Model#fit
+#     history = model.fit(x=nyu_data_generator,
+#                         epochs=10, callbacks=[cp_callback])#(x_val, y_val))
+#
+#     # history = model.fit_generator(nyu_data_generator, steps_per_epoch=5, epochs=1)
+#
+#     print('\nhistory dict:', history.history)
+#
+#     # Evaluate the model on the test data using `evaluate`
+#     # print('\n# Evaluate on test data')
+#     # results = model.evaluate(x_test, y_test, batch_size=128)
+#     # print('test loss, test acc:', results)
+#
+#     # # Generate predictions (probabilities -- the output of the last layer)
+#     # # on new data using `predict`
+#     # print('\n# Generate predictions for 3 samples')
+#     # predictions = model.predict(x_test[:3])
+#     # print('predictions shape:', predictions.shape)
+
 def main():
     print(tf.__version__)
 
-    # Pick a model (coarse or refined)
-    if (is_refine_training):
-        current_filepath = REFINED_CHECKPOINT_PATH,
-        coarse_training_results = models.model2()
-        model = models.refinedNetworkModel(coarse_training_results)
-    else:
-        current_filepath = COARSE_CHECKPOINT_PATH
-        model = models.model2()
+    cp_callback_coarse = tf.keras.callbacks.ModelCheckpoint(filepath=COARSE_CHECKPOINT_PATH,
+                                                     save_weights_only=True,
+                                                     verbose=1)
+    cp_callback_refine = tf.keras.callbacks.ModelCheckpoint(filepath=REFINED_CHECKPOINT_PATH,
+                                                            save_weights_only=True,
+                                                            verbose=1)
 
-    # Create a callback that saves the model's weights with every epoch
-    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=current_filepath, save_weights_only=True,verbose=1, save_freq='epoch')
+    latest_checkpoint_refine = tf.train.latest_checkpoint(REFINED_CHECKPOINT_DIR)
+    latest_checkpoint_coarse = tf.train.latest_checkpoint(COARSE_CHECKPOINT_DIR)
+    refine_model, coarse_model = models.refinedNetworkModel()
+    # if RUN_REFINE:
+    #     refine_model, coarse_model = models.refine_model_def()
+    #     # model = refine_model
+    #     # if latest_checkpoint_refine:
+    #     #     print("\nRestored refine model from checkpoint")
+    #     #     refine_model.load_weights(latest_checkpoint_refine)
+    #     # elif latest_checkpoint_coarse:
+    #     #     print("\nRestored coarse model from checkpoint")
+    #     #     coarse_model.load_weights(latest_checkpoint_coarse)
+    #     # else:
+    #     #     print("\nCoarse model not restored. Please run coarse model first")
+    #     #     return
+    # else:
+    #     coarse_model, _, _ = models.coarse_model_def()
+    #     model = coarse_model
+    #     if latest_checkpoint_coarse:
+    #         print("\nRestored coarse model from checkpoint")
+    #         coarse_model.load_weights(latest_checkpoint_coarse)
+    #     else:
+    #         print("\nNo coarse checkpoint saved")
 
-    latest_checkpoint = tf.train.latest_checkpoint(os.path.dirname(current_filepath))
-
-    # If training refined for the first time, no checkpoint will be found in refined dir.
-    # Restore from coarse directory.
-    if is_refined_training and not latest_checkpoint:
-        latest_checkpoint = tf.train.latest_checkpoint(os.path.dirname(COARSE_CHECKPOINT_PATH))
-        print("\nGetting coarse checkpoint for refined training")
-        model.load_weights(latest_checkpoint)
-    elif (latest_checkpoint): 
-        print("\nRestoring form checkpoint")
-        model.load_weights(latest_checkpoint)
-    else: 
-        print("\nTraining model from scratch")
-        
     nyu_data_generator = NyuDepthGenerator(batch_size=10)
 
-    # parallel_model = multi_gpu_model(model, gpus=2)
-    # parallel_model.compile(optimizer=keras.optimizers.Adam(),  # Optimizer
-    #           # Loss function to minimize
-    #           loss=msr_loss,
-    #           # List of metrics to monitor
-    #           metrics=None)
-
-    model.compile(optimizer=keras.optimizers.Adam(),  # Optimizer
-              # Loss function to minimize
-              loss=depth_loss,
-              # List of metrics to monitor
-              metrics=None)
-
-    print("model metric names: " + str(model.metrics_names))
+    refine_model.compile(optimizer=keras.optimizers.Adam(),  # Optimizer
+                  # Loss function to minimize
+                  loss=depth_loss,
+                  # List of metrics to monitor
+                  metrics=None)
 
     print('# Fit model on training data')
-    # when using data generate, x contains both X and Y. 
-    # batch size is define in the generator thus passing None to batch_size
-    # https://www.tensorflow.org/api_docs/python/tf/keras/Model#fit
-    history = model.fit(x=nyu_data_generator,
-                        epochs=10, callbacks=[cp_callback])#(x_val, y_val))
-
-    # history = model.fit_generator(nyu_data_generator, steps_per_epoch=5, epochs=1)
-
+    if RUN_REFINE:
+        history = refine_model.fit(x=nyu_data_generator,
+                            epochs=300, callbacks=[cp_callback_refine])
+    else:
+        history = coarse_model.fit(x=nyu_data_generator,
+                            epochs=300, callbacks=[cp_callback_coarse])
     print('\nhistory dict:', history.history)
+    np.savetxt("loss_history.txt", history.history["loss"], delimiter=",")
 
-    # Evaluate the model on the test data using `evaluate`
-    # print('\n# Evaluate on test data')
-    # results = model.evaluate(x_test, y_test, batch_size=128)
-    # print('test loss, test acc:', results)
+    result = refine_model.evaluate(x=nyu_data_generator, steps=1)
+    print("test loss: ", result)
 
-    # # Generate predictions (probabilities -- the output of the last layer)
-    # # on new data using `predict`
-    # print('\n# Generate predictions for 3 samples')
-    # predictions = model.predict(x_test[:3])
-    # print('predictions shape:', predictions.shape)
+    if not os.path.isdir(PREDICT_FILE_PATH):
+        os.mkdir(PREDICT_FILE_PATH)
+    predictions = refine_model.predict(x=nyu_data_generator, steps=1)
+    print('predictions shape:', predictions.shape)
+    for i in range(predictions.shape[0]):
+        predictions[i] = (predictions[i] / np.max(predictions[i])) * 255.0
+        image_name = os.path.join(PREDICT_FILE_PATH, '%05d_d.png' % i)
+        image_im = Image.fromarray(np.uint8(predictions[i].reshape(TARGET_HEIGHT, TARGET_WIDTH)), mode="L")
+        image_im.save(image_name)
 
 
 def debug_display_rgbd_pair(rgb, d):
