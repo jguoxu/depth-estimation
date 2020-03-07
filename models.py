036e00dcc5b54f314950f4fb3a7a43e78ff40616
Isha Singhal <isinghal@isinghal-macbookpro.roam.corp.google.com>
2020-03-07 00:21:07 -0800

    Changed the last layer of coarse model

--- models.py
+++ models.py
@@ -2,57 +2,54 @@ from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, Input, MaxPooling2D, InputLayer,UpSampling2D, Reshape,UpSampling1D
 from tensorflow.keras.layers import BatchNormalization, concatenate
 from train import IMAGE_HEIGHT, IMAGE_WIDTH
+from train import TARGET_WIDTH, TARGET_HEIGHT
 
 def coarse_network_model():
-    first_layer=Input(shape=(IMAGE_HEIGHT, IMAGE_WIDTH, 3))
-    conv1 = Conv2D(96,(11,11),strides=(4,4),activation="relu",padding="same")(first_layer)
-    batch1 = BatchNormalization()(conv1)
-    pool1 = MaxPooling2D(pool_size=(2,2))(batch1)
-    
-    conv2 = Conv2D(256, (5, 5), activation="relu", padding="same")(pool1)
-    batch2 = BatchNormalization()(conv2)
-    pool2 = MaxPooling2D(pool_size=(2,2))(batch2)
+    first_layer = Input(shape=(IMAGE_HEIGHT, IMAGE_WIDTH, 3))
+    conv1 = Conv2D(96, (11, 11), strides=(4, 4), activation='relu', padding='same')(first_layer)
+    b1 = BatchNormalization()(conv1)
+    p1 = MaxPooling2D(pool_size=(2, 2))(b1)
 
-    conv3 = Conv2D(384, (3, 3), activation="relu", padding="same")(pool2)
-    batch3 = BatchNormalization()(conv3)
+    conv2 = Conv2D(256, (5, 5), activation='relu', padding='same')(p1)
+    b2 = BatchNormalization()(conv2)
+    p2 = MaxPooling2D(pool_size=(2, 2))(b2)
 
-    conv4 = Conv2D(384, (3, 3), activation="relu", padding="same")(batch3)
-    batch4 = BatchNormalization()(conv4)
+    conv3 = Conv2D(384, (3, 3), activation='relu', padding='same')(p2)
+    b3 = BatchNormalization()(conv3)
 
-    dense5 = Dense(256, activation="relu")(batch4)
-    batch5 = BatchNormalization()(dense5)
-    pool5 = MaxPooling2D(pool_size=(2, 2))(batch5)
+    conv4 = Conv2D(384, (3, 3), activation='relu', padding='same')(b3)
+    b4 = BatchNormalization()(conv4)
 
-    flatten = Flatten()(pool5)
-    flatten = Dense(4096, activation="linear")(flatten)
-    flatten = BatchNormalization()(flatten)
-    flatten = Dropout(0.4)(flatten) 
+    Dlayer1 = Dense(256, activation='relu')(b4)
+    b5 = BatchNormalization()(Dlayer1)
+    p5 = MaxPooling2D(pool_size=(2, 2))(b5)
 
-    reshaped=Reshape((64,64,1))(flatten)
-    upsampled=UpSampling2D(size=(2,2))(reshaped)
+    flat = Flatten()(p5)
+    flat = Dense(4096, activation='relu')(flat)
+    flat = BatchNormalization()(flat)
+    flat = Dropout(0.4)(flat)
 
-    out1 = Conv2D(1, (74, 55), padding="valid")(upsampled)
-    out1 = BatchNormalization()(out1)
-    model = Model(inputs=first_layer, outputs=out1)
-    model.summary
-    return model, out1, first_layer
+    flat = Dense(4070, activation='linear')(flat)
+    flat = BatchNormalization()(flat)
+    coarse_output = Reshape((TARGET_HEIGHT, TARGET_WIDTH, 1))(flat)
+
+    coarse_model = Model(inputs=first_layer, outputs=coarse_output)
+    coarse_model.summary()
+    return coarse_model, coarse_output, first_layer
 
 def refined_network_model():
-    coarse_model, coarse_training_results, layer1 = coarse_network_model()
+    coarse_model, coarse_output, first_layer = coarse_model_def()
+    conv21 = Conv2D(63, (9, 9), strides=(2, 2), padding='valid')(first_layer)
+    b21 = BatchNormalization()(conv21)
+    p21 = MaxPooling2D(pool_size=(2, 2))(b21)
 
-    conv1 = (Conv2D(filters = 63, kernel_size= (9, 9),
-                    strides=(2, 2),
-                    padding="valid"))(layer1)
-    batch1 = BatchNormalization()(conv1)
-    pool1 = MaxPooling2D(pool_size=(2,2))(batch1)
+    Concat = concatenate([coarse_output, p21])
+    conv22 = Conv2D(64, (5, 5), padding='same')(Concat)
+    b22 = BatchNormalization()(conv22)
 
-    merged = concatenate([coarse_training_results, pool1]) 
-    conv2 = Conv2D(filters = 64, kernel_size=(5, 5), padding="same")(merged)
-    batch2 = BatchNormalization()(conv2)
-
-    out = Conv2D(filters = 1, kernel_size=(5, 5), padding="same")(batch2)
+    out = Conv2D(1, (5, 5), padding='same')(b22)
     out = BatchNormalization()(out)
 
-    refined_model = Model(inputs=layer1, outputs=out)
-    refined_model.summary()
-    return refined_model, coarse_model
+    refine_model = Model(inputs=first_layer, outputs=out)
+    refine_model.summary()
+    return refine_model, coarse_model
