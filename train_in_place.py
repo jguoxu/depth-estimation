16ae952e5d997e4c4c0773f2a11b5fd1128b27fc
Meijiao Png <meijiao@gmail.com>
2020-03-08 12:27:43 -0700

    log metrics for inplace training

--- train_in_place.py
+++ train_in_place.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import os.path
 import models
+import metrics
 
 # TensorFlow and tf.keras
 import tensorflow as tf
@@ -11,6 +12,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger
 from tensorflow.keras.utils import multi_gpu_model
+from tensorflow.keras.metrics import RootMeanSquaredError
 
 # from scipy import imageio
 from PIL import Image
@@ -98,7 +100,7 @@ def main():
                   # Loss function to minimize
                   loss=models.depth_loss,
                   # List of metrics to monitor
-                  metrics=None)
+                  metrics=[RootMeanSquaredError(name='keras_default_RMSE'), metrics.scale_invariant_loss, metrics.scale_invariant_mse])
 
     print('Fit model on training data')
     if RUN_REFINE:
