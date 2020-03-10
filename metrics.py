f205142c80eef4d5b131596da2759957e235f2a2
Isha Singhal <isinghal@isinghal-macbookpro.roam.corp.google.com>
2020-03-09 23:56:48 -0700

    Implemented scale invariant metrics

--- metrics.py
+++ metrics.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import math
 import tensorflow.keras.backend as K
+from tensorflow.keras.layers import Reshape
 
 TARGET_HEIGHT = 55
 TARGET_WIDTH = 74
@@ -30,3 +31,27 @@ def squared_relative_diff(y_true, y_pred):
 def rmse(y_true, y_pred):
     d = K.cast(y_pred - y_true, dtype='float32')
     return K.sqrt(K.sum(K.square(d))/N)
+
+def rmse_scale_invariant(y_true, y_pred):
+    y_true = Reshape((55*74, 1))(y_true)
+    y_pred = Reshape((55*74, 1))(y_pred)
+    y_true = K.cast(y_true, dtype='float32')
+    y_pred = K.cast(y_pred, dtype='float32')
+
+    # without discarding infinity pixels, the loss will quickly gets to nan.
+    lnYTrue = tf.where(tf.math.is_inf(y_true), tf.ones_like(y_true), y_true)
+    lnYPred = tf.where(tf.math.is_inf(y_pred), tf.ones_like(y_pred), y_pred)
+
+    invalid_depths = tf.where(y_true <= 0, 0.0, 1.0)
+    lnYTrue = tf.multiply(lnYTrue, invalid_depths)
+    lnYPred = tf.multiply(lnYPred, invalid_depths)
+
+    d_arr = K.cast(lnYTrue - lnYPred, dtype='float32')
+
+    log_diff = K.cast(K.sum(K.square(d_arr), axis=1) / N, dtype='float32')
+    penalty = K.square(K.sum(d_arr, axis=1)) / K.cast(K.square(N), dtype='float32')
+
+    diff = log_diff - penalty
+    rmse = K.sqrt(K.mean(diff))
+
+    return rmse
--- train_in_place.py
+++ train_in_place.py
@@ -120,7 +120,7 @@ def main():
     model.compile(optimizer=keras.optimizers.Adam(),  # Optimizer
                   # Loss function to minimize
                   loss=models.depth_loss,
-                  metrics= [metrics.rmse, metrics.abs_relative_diff, metrics.squared_relative_diff])
+                  metrics=[metrics.rmse_scale_invariant])
 
     predict_while_train = PredictWhileTrain(x_train)
     early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
@@ -128,10 +128,10 @@ def main():
         os.mkdir(TRAIN_PREDICT_FILE_PATH)
     print('Fit model on training data')
     if RUN_REFINE:
-        history = model.fit(x=x_train, y = y_train, validation_data=(x_eval, y_eval),
+        history = model.fit(x=x_train, y=y_train, validation_data=(x_eval, y_eval),
                             epochs=2005, callbacks=[cp_callback_refine, csv_logger, predict_while_train])
     else:
-        history = model.fit(x=x_train, y = y_train, validation_data=(x_eval, y_eval),
+        history = model.fit(x=x_train, y=y_train, validation_data=(x_eval, y_eval),
                             epochs=2005, callbacks=[cp_callback_coarse, csv_logger, predict_while_train])
 
     print('\nHistory dict:', history.history)
