4c7ca784d2e41d736afd3030059a1c6ab7818568
Meijiao Png <meijiao@gmail.com>
2020-03-09 22:15:16 -0700

    update metrics

--- metrics.py
+++ metrics.py
@@ -6,22 +6,23 @@ TARGET_HEIGHT = 55
 TARGET_WIDTH = 74
 N = K.cast(TARGET_HEIGHT * TARGET_WIDTH, dtype='float32')
 
-# set y true to small non zero number if it's zero to avoid division error
-# set y true to 1 if infinity
-def clean_y_true(y_true):
-    y_true = tf.where(y_true == 0, 0.001, y_true)
-    y_true = tf.where(tf.math.is_inf(y_true), 1.0, y_true)
-    return y_true
+# set y true to 1 if it's zero to avoid NaN division error
+# set y pred to 1 if y true was 0 (y true becomes 1 after this block) so
+# that the metric does not take it into account (difference = 0)
+def clean_y_true(y_true, y_pred):
+    y_pred = tf.where(y_true == 0, 1.0, y_pred)
+    y_true = tf.where(y_true == 0, 1.0, y_true)
+    return y_true, y_pred
 
 def abs_relative_diff(y_true, y_pred):
-    y_true = clean_y_true(K.cast(y_true, dtype='float32'))
+    y_true, y_pred = clean_y_true(K.cast(y_true, dtype='float32'), K.cast(y_pred, dtype='float32'))
     d = K.cast(y_pred - y_true, dtype='float32')
     single_diff = K.abs(d) / y_true
     mean_diff = K.sum(single_diff) / N
     return mean_diff
 
 def squared_relative_diff(y_true, y_pred):
-    y_true = clean_y_true(K.cast(y_true, dtype='float32'))
+    y_true, y_pred = clean_y_true(K.cast(y_true, dtype='float32'), K.cast(y_pred, dtype='float32'))
     d = K.cast(y_pred - y_true, dtype='float32')
     single_diff = K.square(K.abs(d)) / y_true
     mean_diff = K.sum(single_diff) / N
