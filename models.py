641734568877658d5227ae309b38a74aed21a4eb
Isha Singhal <isinghal@isinghal-macbookpro.roam.corp.google.com>
2020-03-12 00:14:22 -0700

    add L2 regularization

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, Input, MaxPooling2D, InputLayer,UpSampling2D, Reshape,UpSampling1D
 from tensorflow.keras.layers import BatchNormalization, concatenate
 import tensorflow.keras.backend as K
+from tensorflow.keras import regularizers
 
 from train import IMAGE_HEIGHT, IMAGE_WIDTH
 from train import TARGET_WIDTH, TARGET_HEIGHT
@@ -86,36 +87,36 @@ def depth_loss_2(y_true, y_pred):
 
 def coarse_network_model():
     first_layer = Input(shape=(IMAGE_HEIGHT, IMAGE_WIDTH, 3))
-    conv1 = Conv2D(96, (11, 11), strides=(4, 4), padding='same')(first_layer)
+    conv1 = Conv2D(96, (11, 11), strides=(4, 4), padding='same', kernel_regularizer=regularizers.l2(0.01))(first_layer)
     b1 = BatchNormalization()(conv1)
     a1 = Activation("relu")(b1)
     p1 = MaxPooling2D(pool_size=(2, 2))(a1)
 
-    conv2 = Conv2D(256, (5, 5), padding='same')(p1)
+    conv2 = Conv2D(256, (5, 5), padding='same', kernel_regularizer=regularizers.l2(0.01))(p1)
     b2 = BatchNormalization()(conv2)
     a2 = Activation("relu")(b2)
     p2 = MaxPooling2D(pool_size=(2, 2))(a2)
 
-    conv3 = Conv2D(384, (3, 3), padding='same')(p2)
+    conv3 = Conv2D(384, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01))(p2)
     b3 = BatchNormalization()(conv3)
     a3 = Activation("relu")(b3)
 
-    conv4 = Conv2D(384, (3, 3), padding='same')(a3)
+    conv4 = Conv2D(384, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01))(a3)
     b4 = BatchNormalization()(conv4)
     a4 = Activation("relu")(b4)
 
-    Dlayer1 = Dense(256)(a4)
+    Dlayer1 = Dense(256, kernel_regularizer=regularizers.l2(0.01))(a4)
     b5 = BatchNormalization()(Dlayer1)
     a5 = Activation("relu")(b5)
     p5 = MaxPooling2D(pool_size=(2, 2))(a5)
 
     flat = Flatten()(p5)
-    flat = Dense(4096)(flat)
+    flat = Dense(4096, kernel_regularizer=regularizers.l2(0.01))(flat)
     flat = BatchNormalization()(flat)
     flat = Activation("linear")(flat)
     flat = Dropout(0.4)(flat)
 
-    flat = Dense(4070, activation='relu')(flat)
+    flat = Dense(4070, activation='relu', kernel_regularizer=regularizers.l2(0.01))(flat)
     flat = BatchNormalization()(flat)
     coarse_output = Reshape((TARGET_HEIGHT, TARGET_WIDTH, 1))(flat)
 
@@ -125,15 +126,15 @@ def coarse_network_model():
 
 def refined_network_model():
     coarse_model, coarse_output, first_layer = coarse_network_model()
-    conv21 = Conv2D(63, (9, 9), strides=(2, 2), padding='valid')(first_layer)
+    conv21 = Conv2D(63, (9, 9), strides=(2, 2), padding='valid', kernel_regularizer=regularizers.l2(0.01))(first_layer)
     b21 = BatchNormalization()(conv21)
     p21 = MaxPooling2D(pool_size=(2, 2))(b21)
 
     Concat = concatenate([coarse_output, p21])
-    conv22 = Conv2D(64, (5, 5), padding='same')(Concat)
+    conv22 = Conv2D(64, (5, 5), padding='same', kernel_regularizer=regularizers.l2(0.01))(Concat)
     b22 = BatchNormalization()(conv22)
 
-    out = Conv2D(1, (5, 5), padding='same')(b22)
+    out = Conv2D(1, (5, 5), padding='same', kernel_regularizer=regularizers.l2(0.01))(b22)
     out = BatchNormalization()(out)
 
     refine_model = Model(inputs=first_layer, outputs=out)
